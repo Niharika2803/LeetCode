a8e2452935bda5a0d73166c68837b7af2eef8cf6
Niharika Mishra <niharikamishra2803@gmail.com>
2024-05-22 00:00:23 +0530

    128. Longest Consecutive Sequence
    Problem Link: https://leetcode.com/problems/longest-consecutive-sequence/description/

--- Solutions/LongestConsecutiveSequence.java
+++ Solutions/LongestConsecutiveSequence.java
@@ -32,11 +32,11 @@ public class LongestConsecutiveSequence {
     // user inputs
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array:");
+        System.out.print("Enter the size of the array: ");
         int arraySize = sc.nextInt();
         int[] nums = new int[arraySize];
 
-        System.out.println("Enter the elements of the array:");
+        System.out.print("Enter the elements of the array: ");
         for (int i = 0; i < arraySize; i++) {
             nums[i] = sc.nextInt();
         }
