30bbaa608cf0d3c0132fec619da0d32749f3a4ae
Niharika Mishra <niharikamishra2803@gmail.com>
2024-05-28 23:58:32 +0530

    11. Container With Most Water
    Problem Link: https://leetcode.com/problems/container-with-most-water/description/

--- Solutions/ContainerWithMostWater.java
+++ Solutions/ContainerWithMostWater.java
@@ -1,23 +1,42 @@
+import java.util.Scanner;
+
 public class ContainerWithMostWater {
-    public int maxArea(int[] height) {
+    // T.C. = O(n)
+    // S.C. = O(1)
+    public static int maxArea(int[] height) {
         int left = 0;
         int right = height.length - 1;
         int res = 0;
 
-        while (left < right){
+        while (left < right) {
             int containerLength = right - left;
-            int area = containerLength * Math.min (height[left], height[right]);
+            int area = containerLength * Math.min(height[left], height[right]);
 
             res = Math.max(res, area);
 
-            if(height[left] < height[right]){
+            if (height[left] < height[right]) {
                 left++;
-            }
-            else{
+            } else {
                 right--;
             }
         }
 
         return res;
+    }
+
+
+
+
+    //user inputs
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of containers: ");
+        int n = sc.nextInt();
+        int[] height = new int[n];
+        System.out.print("Enter the heights of the containers: ");
+        for (int i = 0; i < n; i++) {
+            height[i] = sc.nextInt();
+        }
+        System.out.println("The maximum water that can be stored is " + maxArea(height));
     }
 }
